7b473f42562a71e89b40eda966a3ecba7dec1aec
Yoonasea <phuazaiwei@gmail.com>
2024-08-26 11:37:14 +0800

    first

---
+++ src/components/LinkItem.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Button } from '@mui/material';
+
+interface LinkItemProps {
+  label: string;
+  url: string;
+}
+
+const LinkItem: React.FC<LinkItemProps> = ({ label, url }) => {
+  return (
+    <Button 
+      variant="contained" 
+      href={url} 
+      target="_blank" 
+      sx={{ width: '100%', padding: 2 }}
+    >
+      {label}
+    </Button>
+  );
+};
+
+export default LinkItem;
---
+++ src/components/LinkTree.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Container, Typography, Stack } from '@mui/material';
+import LinkItem from './LinkItem';
+
+interface Link {
+  label: string;
+  url: string;
+}
+
+const LinkTree: React.FC = () => {
+  const links: Link[] = [
+    { label: 'My Portfolio', url: 'https://myportfolio.com' },
+    { label: 'GitHub', url: 'https://github.com/myprofile' },
+    { label: 'LinkedIn', url: 'https://linkedin.com/in/myprofile' },
+    { label: 'Twitter', url: 'https://twitter.com/myprofile' },
+  ];
+
+  return (
+    <Container sx={{ textAlign: 'center', marginTop: 4 }}>
+      <Typography variant="h4" gutterBottom>
+        My Linktree
+      </Typography>
+      <Stack spacing={2}>
+        {links.map((link, index) => (
+          <LinkItem key={index} label={link.label} url={link.url} />
+        ))}
+      </Stack>
+    </Container>
+  );
+};
+
+export default LinkTree;
