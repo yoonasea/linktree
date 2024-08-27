53ef06b00e0d813925c6288d00e7f05428ad1a03
Yoonasea <phuazaiwei@gmail.com>
2024-08-27 17:50:54 +0800

    update links

--- src/components/LinkTree.tsx
+++ src/components/LinkTree.tsx
@@ -11,13 +11,13 @@ const LinkTree: React.FC = () => {
   const links: Link[] = [
     { label: "LinkedIn", url: "https://www.linkedin.com/in/phuazaiwei/" },
     { label: "GitHub", url: "https://github.com/yoonasea" },
-    { label: "Bus Timing App", url: "https://transport-887be.web.app/" },
+    { label: "Weather App", url: "https://transport-887be.web.app/weather" },
     { label: "Drawing App", url: "https://transport-887be.web.app/paint" },
-    { label: "Weather App >wip<", url: "" },
-    { label: "Task Management App >wip<", url: "" },
+    { label: "Bus Timing App", url: "https://transport-887be.web.app/" },
     { label: "3D Printing", url: "https://www.instagram.com/phuazaiwei/" },
     { label: "Chrome Extension", url: "https://chromewebstore.google.com/detail/new-tab-league-of-legends/nokfioegpdopldlbbbhglnjjbgddbcjk" },
   ];
+  // { label: "Task Management App >wip<", url: "" },
 
   return (
     <Container sx={{ textAlign: "center", marginTop: 4 }}>
